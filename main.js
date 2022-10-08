4e03372df5df624de86dd50564edeaa725214264
Oanh Nguyen <myspamvn@yahoo.com>
2022-10-08 23:59:18 +0900

    feat: add Add to cart function

--- main.js
+++ main.js
@@ -1,23 +1,20 @@
 const app = Vue.createApp({
   data() {
-    return {
-      product: 'Socks',
-      image: './assets/images/socks_blue.jpg',
-      inStock: true,
-      details: ['50% cotton', '30% wool', '20% polyester'],
-      variants: [
-        {id: 2234, color: 'green'},
-        {id: 2235, color: 'blue'}
-      ],
-      sizes: [
-        {id: 1, text: 'XXS'},
-        {id: 1, text: 'XS'},
-        {id: 1, text: 'S'},
-        {id: 1, text: 'M'},
-        {id: 1, text: 'L'},
-        {id: 1, text: 'XL'},
-        {id: 1, text: 'XXL'}
-       ]
+      return {
+          cart:0,
+          product: 'Socks',
+          image: './assets/images/socks_blue.jpg',
+          inStock: true,
+          details: ['50% cotton', '30% wool', '20% polyester'],
+          variants: [
+            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+          ]
+      }
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
     }
   }
 })
