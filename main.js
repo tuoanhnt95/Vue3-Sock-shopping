99d48fb22ac2995a77dd55c288f498ce05fd4faf
Oanh <myspamvn@yahoo.com>
2022-10-10 23:18:30 +0900

    feat: Refactor components and props

--- main.js
+++ main.js
@@ -1,38 +1,10 @@
 const app = Vue.createApp({
   data() {
       return {
-          cart:0,
-          product: 'Socks',
-          brand: 'Vue Mastery',
-          onSale: false,
-          selectedVariant: 0,
-          details: ['50% cotton', '30% wool', '20% polyester'],
-          variants: [
-            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-          ]
+          cart: 0,
+          premium: false,
+          details: 'Footwear for cold feet'
       }
   },
-  methods: {
-      addToCart() {
-          this.cart += 1
-      },
-      updateVariant(index) {
-          this.selectedVariant = index
-      }
-  },
-  computed: {
-    title() {
-      return this.brand + " " + this.product 
-    },
-    image() {
-      return this.variants[this.selectedVariant].image
-    },
-    inStock() {
-      return this.variants[this.selectedVariant].quantity
-    },
-    saleItem() {
-      return this.onSale ? " is on sale" : ""
-    }
-  }
+  methods: {  }
 })
