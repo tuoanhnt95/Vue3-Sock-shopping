1f82d0c8cc7dc0080e9afcd637160c91b42b5e09
Oanh Nguyen <ONguyen@HTMJPN.local>
2022-10-11 18:34:44 +0900

    feat: Add emit events to pass changes to other components

--- main.js
+++ main.js
@@ -1,23 +1,16 @@
 const app = Vue.createApp({
   data() {
-    return {
-      product: 'Socks',
-      image: './assets/images/socks_blue.jpg',
-      inStock: true,
-      details: ['50% cotton', '30% wool', '20% polyester'],
-      variants: [
-        {id: 2234, color: 'green'},
-        {id: 2235, color: 'blue'}
-      ],
-      sizes: [
-        {id: 1, text: 'XXS'},
-        {id: 1, text: 'XS'},
-        {id: 1, text: 'S'},
-        {id: 1, text: 'M'},
-        {id: 1, text: 'L'},
-        {id: 1, text: 'XL'},
-        {id: 1, text: 'XXL'}
-       ]
+      return {
+          cart: [],
+          premium: true
+      }
+  },
+  methods: {
+    updateCart(id) {
+      this.cart.push(id)
+    },
+    removeCart() {
+      this.cart.pop()
     }
   }
 })
